28db93ce365a74c4c40a4931205ac0141af29c55
Dmitry Bravikov <dmitry@bravikov.pro>
2018-01-27 18:18:34 +0500

    Add test.

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat url_list | ./markov_chain_generator 1 | tee markov_chain
---
+++ test/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+make
+cd test
+cp -R mc_texts /tmp
+cat url_list | ../markov_chain_generator 1 | tee markov_chain
